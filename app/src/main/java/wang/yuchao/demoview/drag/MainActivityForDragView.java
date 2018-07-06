1ebac3fab31581ff0cd7692f105d3446eed57536
yuchao-wang <1154786190@qq.com>
2018-07-06 16:47:05 +0800

    MOD:拖动视图 BUG 修复：快速拖动紊乱+快速拖动空白

--- app/src/main/java/wang/yuchao/demoview/drag/MainActivityForDragView.java
+++ app/src/main/java/wang/yuchao/demoview/drag/MainActivityForDragView.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.DragEvent;
 import android.view.View;
 import android.view.ViewGroup;
@@ -77,15 +78,31 @@ public class MainActivityForDragView extends Activity {
                     }
                     break;
                     case DragEvent.ACTION_DRAG_LOCATION: {
+                        // 注意：快速拖动时由于来不及向每个经过的视图发送消息，因此会造成顺序紊乱的问题
+                        // 解决：递归交换视图
                         if (view != dragView) {
-                            swapChild(viewGroup, dragView, view);
+                            // 递归调用或者循环解决拖动问题
+                            swapViews(viewGroup, dragState, view);
+                            // swapViews2(viewGroup, dragState, view);
+
+                            // 注意：直接交换会出现快速拖动 BUG
+                            // swapChild(viewGroup, dragState, view);
                         }
                     }
                     break;
-                    case DragEvent.ACTION_DROP:
+
+                    case DragEvent.ACTION_DROP: {
+                        Button button = (Button) view;
+                        Log.e(TAG, ">>>>>>>>>>> DROP: view 针对 DragView " + button.getText());
+                        showAllView(viewGroup);
+                    }
+                    break;
                     case DragEvent.ACTION_DRAG_ENDED: {
+                        Button button = (Button) view;
+                        Log.e(TAG, ">>>>>>>>>>> END: view 针对 TargetView " + button.getText());
                         if (view == dragState.view) {
-                            view.setVisibility(View.VISIBLE);
+                            // 注意：不知道为啥 showAllView 必须要调用两次
+                            showAllView(viewGroup);
                         }
                     }
                     break;
@@ -95,10 +112,60 @@ public class MainActivityForDragView extends Activity {
         });
     }
 
-    // 交换两个子 View
-    private void swapChild(ViewGroup viewGroup, View dragView, View targetView) {
+    // 循环交换
+    private void swapViews2(ViewGroup viewGroup, DragState dragState, View targetView) {
         int targetIndex = viewGroup.indexOfChild(targetView);
+        int dragIndex = dragState.index;
+
+        if (targetIndex > dragIndex) {
+            for (int i = dragIndex; i < targetIndex; i++) {
+                swapChild(viewGroup, dragState, viewGroup.getChildAt(i + 1));
+            }
+        } else {
+            for (int i = dragIndex; i > targetIndex; i--) {
+                swapChild(viewGroup, dragState, viewGroup.getChildAt(i - 1));
+            }
+        }
+    }
+
+    private void showAllView(ViewGroup viewGroup) {
+        // 所有恢复显示
+        for (int i = 0; i < viewGroup.getChildCount(); i++) {
+            View view = viewGroup.getChildAt(i);
+            boolean visible = (view.getVisibility() == View.VISIBLE);
+            Log.e(TAG, "index:" + i + " >>> visible:" + visible);
+            if (!visible) {
+                view.setVisibility(View.VISIBLE);
+            }
+        }
+    }
+
+    // 递归交换
+    private void swapViews(ViewGroup viewGroup, DragState dragState, View targetView) {
+        // 递归判断是否要交换
+        swapChildren(viewGroup, dragState, targetView);
+        // 交换操作
+        swapChild(viewGroup, dragState, targetView);
+    }
+
+    // 先交换其他的
+    private void swapChildren(ViewGroup viewGroup, DragState dragState, View targetView) {
+        int targetIndex = viewGroup.indexOfChild(targetView);
+        int dragIndex = dragState.index;
+
+        // 判断两边距离是否大于1
+        if ((targetIndex - dragIndex) > 1) {
+            swapViews(viewGroup, dragState, viewGroup.getChildAt(targetIndex - 1));
+        } else if ((dragIndex - targetIndex) > 1) {
+            swapViews(viewGroup, dragState, viewGroup.getChildAt(targetIndex + 1));
+        }
+    }
+
+    // 交换两个子 View
+    private void swapChild(ViewGroup viewGroup, DragState dragState, View targetView) {
+        View dragView = dragState.view;
         int dragIndex = viewGroup.indexOfChild(dragView);
+        int targetIndex = viewGroup.indexOfChild(targetView);
 
         float viewY = targetView.getY();
 
@@ -113,6 +180,9 @@ public class MainActivityForDragView extends Activity {
             viewGroup.addView(dragView, targetIndex);   // 添加 index 大的
             viewGroup.addView(targetView, dragIndex);   // 添加 index 小的
         }
+
+        // 交换 index
+        dragState.index = targetIndex;
 
         // 添加动画
         runAnimation(targetView, viewY);
@@ -129,7 +199,7 @@ public class MainActivityForDragView extends Activity {
                 }
                 ObjectAnimator
                         .ofFloat(view, View.Y, viewY, view.getTop())
-                        .setDuration(300)
+                        .setDuration(250)
                         .start();
                 return true;
             }
